8b13a71d17ee66176042ba304b7994ba67d8ce2a
SudityaKulkarni <suditya.kulkarni@gmail.com>
2025-07-29 22:33:28 +0530

    tailwindcss

---
+++ 03_project/src/components/card.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Card(){
+    return(
+        <div className="max-w-xs p-6 rounded-md shadow-md bg-black">
+        <img
+          src="https://plus.unsplash.com/premium_photo-1751667124857-32b5a1c63d8a?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw2fHx8ZW58MHx8fHx8"
+          alt=""
+          className="object-cover object-center w-full rounded-md h-72 bg-gray-500"
+        />
+        <div className="mt-6 mb-2">
+          <span className="block text-sm font-medium font-mono tracking-widest uppercase text-indigo-400">
+            Title
+          </span>
+          <h2 className="text-xl font-semibold tracking-wide">Lorem ipsum dolor</h2>
+        </div>
+        <p className="text-gray-300">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio tempora ipsum soluta
+          amet
+        </p>
+      </div>
+    )
+}
+
+export default Card 
